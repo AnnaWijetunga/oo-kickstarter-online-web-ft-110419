9a673721a70a60dc929df4159049ede71fa1c38d
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-20 13:55:54 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,12 +10,14 @@ class Project
   
   def add_backer(backer)
     @backers << backer 
+    backer.
   end 
+  
+  def
+
+  #   project.add_backer(self) unless project.backers.include?(self) # adds the backer to the project's backers array
   
 end 
 
-# once both classes have their attributes and readers set up, write a method on the Project class call add_backer() that takes in a Backer instance and adds the backer to its @backers attribute
-
 # whenever back_project() is called to update @backed_projects, the project should also update its @backers list
-
 # whenever add_backer()  is called to update @backers, the backer should also update its @backed_projects list 
