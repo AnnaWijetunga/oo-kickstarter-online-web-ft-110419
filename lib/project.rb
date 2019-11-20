7078f4619f01c314cf46b7f451339e88efa5f38a
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-20 14:10:22 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -14,6 +14,3 @@ class Project
   end 
   
 end 
-
-# whenever back_project() is called to update @backed_projects, the project should also update its @backers list
-# whenever add_backer()  is called to update @backers, the backer should also update its @backed_projects list 
