e37c2fd91becff2ebab66405a7788a241cb4f910
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-20 13:59:43 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -12,10 +12,11 @@ class Project
     @backers << backer 
     backer.
   end 
-  
-  def
 
-  #   project.add_backer(self) unless project.backers.include?(self) # adds the backer to the project's backers array
+  def back_project(project)
+    @backed_projects << project # accepts a Project as an arugment and stores it in a back_projects array
+    project.add_backer(self) unless project.backers.include?(self) # adds the backer to the project's backers array
+  end 
   
 end 
 
